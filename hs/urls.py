4706c2ede8078117dc4271d5e7ae357ed66d8318
cyh <lalala@xxx.com>
2020-01-19 21:20:49 +0800

    better views

--- hs/urls.py
+++ hs/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<str:card_name>/', views.detail, name='detail'),
 ]
--- hs/views.py
+++ hs/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Minion, Magic, Dk, Weapon, Version
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the hs index.")
+    all_minions = list(Minion.objects.all())
+    all_magics = list(Magic.objects.all())
+    all_dks = list(Dk.objects.all())
+    all_weapons = list(Weapon.objects.all())
+    all_cards = all_minions + all_magics + all_dks + all_weapons
+    output = '\n'.join([c.name for c in all_cards])
+    return HttpResponse(output)
+
+
+def detail(request, card_name):
+    return HttpResponse("You're looking at card '%s'." % card_name)
