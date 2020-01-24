585bf1fecda0fda216d962f801601068d844bd09
cyh <lalala@xxx.com>
2020-01-24 10:51:40 +0800

    detail视图拥有基本元素，更为细致

--- hs/views.py
+++ hs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from pydoc import locate
-from .models import Card, Version
+from .models import Card, Version, JOB_CHOICE, RARITY_CHOICE
 
 
 def index(request):
@@ -13,8 +13,21 @@ def index(request):
 
 def detail(request, real_type_name, real_id):
     real_type = locate('hs.models.' + real_type_name)
-    card_name = get_object_or_404(real_type, pk=real_id)
+    card = get_object_or_404(real_type, pk=real_id)
+    job_value = card.job
+    rarity_value = card.rarity
+    pub_version_value = card.pub_version
+    job = JOB_CHOICE.__getitem__(job_value - 1)[1]
+    rarity = RARITY_CHOICE.__getitem__(rarity_value - 1)[1]
+    cost = card.cost
+    effect = card.effect
+    explanation = card.explanation
     context = {
-        'card_name': card_name
+        'card_name': card.name,
+        'card_job': job,
+        'card_rarity': rarity,
+        'card_cost': cost,
+        'card_effect': effect,
+        'card_explanation': explanation
     }
     return render(request, 'hs/detail.html', context)
