1ae2b48a24b19ddcf28c8825c2c283d53fcb8a41
cyh <lalala@xxx.com>
2020-02-16 19:05:59 +0800

    start learning js

--- polls/views.py
+++ polls/views.py
@@ -42,3 +42,7 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+def guess_number(request):
+    return render(request, 'polls/guess_number.html')
